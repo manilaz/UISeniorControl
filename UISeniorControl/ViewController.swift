06d80f84e9124879a4c1ec86c6f2bdcfec4d9e15
manilaz <24566370@qq.com>
2018-07-13 18:12:22 +0800

    test

--- UISeniorControl/ViewController.swift
+++ UISeniorControl/ViewController.swift
@@ -23,8 +23,26 @@ class ViewController: UIViewController {
         btn.frame = CGRect(x: 20, y: 100, width: 280, height: 30)
         btn.setTitle("push", for: .normal)
         btn.addTarget(self, action: #selector(push), for: .touchUpInside)
+        //navigationController?.navigationBar.setBackgroundImage(UIImage(named: "head"), for: .default)
+        navigationController?.navigationBar.barTintColor = .purple
         
+        let btnItem1 = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(click))
+        btnItem1.setValue( "1", forKey: "title")
+        let btnItem2 = UIBarButtonItem(barButtonSystemItem: .reply, target: self, action: #selector(click))
+//        btnItem2.setValue( "2", forKey: "title")
+//        navigationItem.leftBarButtonItems = [btnItem1,btnItem2]
+//        navigationItem.backBarButtonItem = btnItem2
+        navigationController?.navigationItem.leftBarButtonItem = btnItem1
+//        navigationItem.backBarButtonItem
+//        navigationItem.leftBarButtonItem = btnItem2
+        navigationController?.navigationItem.hidesBackButton = true
         view.addSubview(btn)
+    }
+    
+    @objc func click(btn:UIBarButtonItem){
+        
+        print(btn.value(forKey : "title"))
+        
     }
     
     @objc func push() {
@@ -32,6 +50,7 @@ class ViewController: UIViewController {
         let con = ViewController()
         con.title = "第\(count)个视图"
         navigationController?.pushViewController(con, animated: true)
+        
         print(count)
     }
 
