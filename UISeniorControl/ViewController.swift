83431747aa9cc9c1441038fca1f06678f2bafbbf
manilaz <24566370@qq.com>
2018-07-13 10:02:03 +0800

    senior control

--- UISeniorControl/ViewController.swift
+++ UISeniorControl/ViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
+var count = 1
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
+        title = "第\(count)个视图"
+        count += 1
+        let btn = UIButton(type: .system)
+        btn.frame = CGRect(x: 20, y: 100, width: 280, height: 30)
+        btn.setTitle("push", for: .normal)
+        btn.addTarget(self, action: #selector(push), for: .touchUpInside)
+        
+        view.addSubview(btn)
+    }
+    
+    @objc func push() {
+        
+        let con = ViewController()
+        con.title = "第\(count)个视图"
+        navigationController?.pushViewController(con, animated: true)
+        print(count)
     }
 
     override func didReceiveMemoryWarning() {
